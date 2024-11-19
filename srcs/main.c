fcc6ecb62f6e42834e2034e5f7abe71fc550fa93
Simon Peladeau <speladea@made-f0Ar10s3.clusters.42paris.fr>
2024-11-19 14:50:01 +0100

    add prompt

--- srcs/main.c
+++ srcs/main.c
@@ -1,6 +1,20 @@
 #include <../includes/minishell.h>
 
-int main(int arc, char **argv, char **envp)
+int main(void)
 {
-	
+	char	*prompt;
+
+	while (1)
+	{
+		prompt = readline("GhostInTheShell> ");
+		if (prompt == NULL)
+		{
+			printf("\nExiting.\n");
+			break ;
+		}
+		if (*prompt != '\0')
+			add_history(prompt);
+		printf("%s\n", prompt);
+		free(prompt);
+	}
 }
