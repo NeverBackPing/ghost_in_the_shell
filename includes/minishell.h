f28532e15d0a0da3c2f8ba5200ad2ca3316ca57c
Simon Peladeau <speladea@made-f0Ar10s3.clusters.42paris.fr>
2024-11-19 18:34:32 +0100

    end of day 1

--- includes/minishell.h
+++ includes/minishell.h
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: speladea <speladea@student.42.fr>          #+#  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024-11-19 16:46:01 by speladea          #+#    #+#             */
+/*   Updated: 2024-11-19 16:46:01 by speladea         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
@@ -7,5 +19,21 @@
 # include <stdlib.h>
 # include <readline/readline.h>
 # include <readline/history.h>
+
+typedef struct s_token
+{
+	int	cmd;
+	int	pipe;
+	int	red_input;
+	int	red_output;
+}	t_token;
+
+typedef struct s_pars
+{
+
+}	t_pars;
+
+void	ft_check_prompt(char *prompt);
+
 
 #endif
